f5a8a86080407b2fda57f619ea45540bc43d6a48
pray3m <prem.gtm9@gmail.com>
2024-07-08 07:23:26 +0545

    ✨create popup menu

--- AndroidMenus/app/src/main/java/com/example/androidmenus/MainActivity.java
+++ AndroidMenus/app/src/main/java/com/example/androidmenus/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -43,5 +46,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonShowPopup = findViewById(R.id.buttonShowPopup);
+        buttonShowPopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.menu_edit) {
+                    Toast.makeText(MainActivity.this, "Edit selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (itemId == R.id.menu_delete) {
+                    Toast.makeText(MainActivity.this, "Delete selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (itemId == R.id.menu_share) {
+                    Toast.makeText(MainActivity.this, "Share selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        popupMenu.show();
     }
 }
