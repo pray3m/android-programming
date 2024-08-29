dc4f1af4f79c19421a2356ce2796299595198a03
pray3m <prem.gtm9@gmail.com>
2024-08-29 11:51:42 +0545

    feat: add relative layout and table layout

--- LayoutExplorer/app/src/main/java/com/example/MainActivity.java
+++ LayoutExplorer/app/src/main/java/com/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.example;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -16,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
         Button btnLinear = findViewById(R.id.btn_linear);
         btnLinear.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnRelative = findViewById(R.id.btn_relative);
+        btnRelative.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
+            startActivity(intent);
+        });
+
+
+        Button btnTable = findViewById(R.id.btn_table);
+        btnTable.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TableLayoutActivity.class);
             startActivity(intent);
         });
 
